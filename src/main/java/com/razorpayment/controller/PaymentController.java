d596e15d811f4dd34be60e248c02f3c73ebd04a7
anamikarai460 <anamikarai460@gmail.com>
2024-02-13 10:21:43 +0530

    Commit Pay one

---
+++ src/main/java/com/razorpayment/controller/PaymentController.java
@@ -0,0 +1,34 @@
+package com.razorpayment.controller;
+
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import com.razorpayment.payload.OrderRequest;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api")
+public class PaymentController {
+
+   // http://localhost:8080/api/create-order
+    @PostMapping("/create-order")
+    public String createOrder(@RequestBody OrderRequest order) throws RazorpayException {
+        RazorpayClient razorpay = new RazorpayClient("rzp_test_1j2WgtDgY0EtKz", "FX7qLf5ByGUHoq2UJ8wn3LNL");
+
+        JSONObject orderRequest = new JSONObject();
+        orderRequest.put("amount",order.getAmount()*100);
+        orderRequest.put("currency","INR");
+        orderRequest.put("receipt", "receipt#1");
+        JSONObject notes = new JSONObject();
+        notes.put("notes_key_1","Tea, Earl Grey, Hot");
+        notes.put("notes_key_1","Tea, Earl Grey, Hot");
+        orderRequest.put("notes",notes);
+
+        Order orderResponse = razorpay.orders.create(orderRequest);
+        System.out.println(orderResponse.get("id").toString());
+        return orderResponse.get("id").toString();
+    }
+}
---
+++ src/main/java/com/razorpayment/payload/OrderRequest.java
@@ -0,0 +1,7 @@
+package com.razorpayment.payload;
+import lombok.Data;
+@Data
+public class OrderRequest {
+    private long amount;
+    private String currency;
+}
